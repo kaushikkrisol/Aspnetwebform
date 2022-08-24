72a583273965f57d9033c9539dc6220add93b8a2
kaushikkrisol <51433140+kaushikkrisol@users.noreply.github.com>
2022-08-24 16:00:08 -0700

    added webconfig information

--- Sampleapp/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sampleapp.Global" Language="C#" %>
---
+++ Sampleapp/testmodule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Sampleapp
+{
+    public class testmodule : IHttpModule
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Contextrequestcompletedfile");
+        }
+    }
+}
